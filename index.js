20fb039bd52e050adcd11e379d4a69f4bbbd4466
Susan Tan <susan.tan198@gmail.com>
2019-01-28 21:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function happyHoliday(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  return `Happy holiday, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdow(holiday, days){
+  return `It's ${days} days until ${holiday}`
+}
 
