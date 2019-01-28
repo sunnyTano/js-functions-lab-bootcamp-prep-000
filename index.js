324d4cac9dc19e9702a5945c0b26ee9329ce4237
Susan Tan <susan.tan198@gmail.com>
2019-01-28 21:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
 }
 
 function holidayCountdown(holiday, days){
-  return `It\'s ${days} days until ${holiday}`
+  return `It\'s ${days} days until ${holiday}!`
 }
 
